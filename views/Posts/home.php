0f239995eec99628a4e4e72d368196d7e4095917
MuhammadDanish <danishbutt.uos@gmail.com>
2022-02-20 22:18:28 +0100

    Yii Two

---
+++ views/Posts/home.php
@@ -0,0 +1,51 @@
+
+<?php
+echo "here";
+die();
+?>
+
+<div class="site-index">
+
+    <div class="jumbotron text-center bg-transparent">
+        <h1 class="display-4">Yii 2 CRUD Operation!!</h1>
+
+    </div>
+
+    <div class="body-content">
+
+        <div class="row">
+        <table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Description</th>
+      <th scope="col">Category</th>
+      <th scope="col">Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      <th scope="row">Active</th>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>Column content</td>
+      <td>
+          <span>
+              <button type="button" name="viewBtn">View</button>
+            </span>
+          <span>
+              <button type="button" name="updateBtn">Update</button>
+            </span>
+          <span>
+              <button type="button" name="delBtn">Delete</button>
+            </span>
+      </td>
+    </tr>
+   
+  </tbody>
+</table>
+        </div>
+
+    </div>
+</div>
---
+++ views/site/index.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var yii\web\View $this */
+
+$this->title = 'My Yii Application';
+?>
+
+    <div class="body-content">
+
+        <div class="row">
+            <h5>Yii 2.0</h5>
+        </div>
+    </div>
